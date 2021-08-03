8607fa5ea6c107ca562c6540815667c8154f694e
Chayanitoey <61849099+Chayanitoey@users.noreply.github.com>
2021-08-03 23:53:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,172 +1,78 @@
-'use strict';
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  const fileAttachments = new Map([["alphabet.csv",new URL("./files/09f63bb9ff086fef80717e2ea8c974f918a996d2bfa3d8773d3ae12753942c002d0dfab833d7bee1e0c9cd358cd3578c1cd0f9435595e76901508adc3964bbdc",import.meta.url)]]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], function(md){return(
+md`# Bar Chart
 
-class Slice {
-	constructor(lbl, val, strt, fll) {
-		this.label = lbl;
-		this.value = constrain(val, 0.0, 1.0);
-		this.start = constrain(strt, 0.0, TWO_PI);
-		var valAngle = map(this.value, 0.0, 1.0, 0.0, TWO_PI);
-		this.middle = this.start + valAngle * 0.5;
-		this.stop = this.start + valAngle;
-		this.fillFocus = color(fll, 99, 99, 255);
-		this.fillNoFocus = color(fll, 89, 49, 189);
-		this.fill = this.fillNoFocus;
-		this.hasFocus = false;
-		this.labelInset = 0.667;
-	}
+This chart shows the relative frequency of letters in the English language. This is a vertical bar chart, also known as a *column* chart. Compare to a [horizontal bar chart](/@d3/horizontal-bar-chart).`
+)});
+  main.variable(observer("chart")).define("chart", ["d3","width","height","color","data","x","y","xAxis","yAxis"], function(d3,width,height,color,data,x,y,xAxis,yAxis)
+{
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height]);
 
-	toString() {
-		return this.label + ": " + (this.value * 100).toFixed(2) + "%";
-	}
+  svg.append("g")
+      .attr("fill", color)
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("y", d => y(d.value))
+      .attr("height", d => y(0) - y(d.value))
+      .attr("width", x.bandwidth());
 
-	draw(cx, cy, radius) {
-		if (this.hasFocus) {
-			this.fill = lerpColor(this.fill,
-					this.fillFocus,
-					Slice.focusFadeIn);
-		} else {
-			this.fill = lerpColor(this.fill,
-					this.fillNoFocus,
-					Slice.focusFadeOut);
-		}
+  svg.append("g")
+      .call(xAxis);
 
-		fill(this.fill);
-		arc(cx, cy, radius, radius,
-			this.start, this.stop);
-	}
+  svg.append("g")
+      .call(yAxis);
 
-	showLabel(cx, cy, radius) {
-		var x = cx + cos(this.middle) * radius * this.labelInset;
-		var y = cy + sin(this.middle) * radius * this.labelInset;
-		fill(0, 0, 0);
-		text(this, x, y);
-		fill(0, 0, 99);
-		text(this, x - 1, y - 1);
-	}
-
-	// atan2 returns a value in the range of -PI to PI.
-	// If the angle is less than 0 radians, we can fix the
-	// problem by adding TWO_PI to it.
-	hover(angle) {
-		angle = angle < 0 ? angle + TWO_PI : angle;
-		return angle > this.start && angle < this.stop;
-	}
+  return svg.node();
 }
-
-Slice.focusFadeIn = 0.05;
-Slice.focusFadeOut = 0.025;
-
-class PieChart {
-	constructor(x, y, rad, name, data) {
-		this.scale(x, y, rad);
-		this.hasFocus = false;
-		this.name = name;
-
-		// Assumes data is an ES6 map.
-		// Sum the values in the data so as to
-		// normalize them to a percent.
-		var sum = 0;
-		var itr = data.entries();
-		for (var value of data.values()) {
-			sum += value;
-			console.log(value);
-		}
-		sum = sum <= 1 ? 1 : sum;
-		console.log("sum: ", sum);
-
-		this.slices = [];
-		var i = 0;
-		for (var[key, value] of data.entries()) {
-			this.slices.push(new Slice(
-					key,
-					isNaN(value) ? 0 : value / sum,
-					i === 0 ? 0 : this.slices[i - 1].stop,
-					i / data.size * 360));
-			i++;
-		}
-	}
-
-	draw() {
-		push();
-		noStroke();
-		ellipseMode(RADIUS);
-    textSize(16);
-		textAlign(CENTER, CENTER);
-		for (var i = 0, size = this.slices.length; i < size; ++i) {
-			this.slices[i].draw(this.x, this.y, this.radius);
-		}
-		if (this.hasFocus) {
-			var y = this.y + this.radius + 16;
-			fill(0, 0, 0);
-			text(this.name, this.x, y);
-			fill(0, 0, 99);
-			text(this.name, this.x - 1, y - 1);
-		}
-		pop();
-		this.hover(mouseX, mouseY);
-
-	}
-
-	hover(x, y) {
-		if (dist(x, y, this.x, this.y) < this.radius) {
-			this.hasFocus = true;
-			for (var i = 0, size = this.slices.length; i < size; ++i) {
-				var a = atan2(y - this.y, x - this.x);
-				if (this.slices[i].hover(a)) {
-					this.slices[i].hasFocus = true;
-					this.slices[i].showLabel(this.x,
-						this.y,
-						this.radius);
-				} else {
-					this.slices[i].hasFocus = false;
-				}
-			}
-		} else {
-			this.hasFocus = false;
-			for (var i = 0, size = this.slices.length; i < size; ++i) {
-				this.slices[i].hasFocus = false;
-			}
-		}
-	}
-  
-  scale(x, y, rad) {
-		this.x = x;
-		this.y = y;
-		this.radius = rad;
-	}
+);
+  main.variable(observer("data")).define("data", ["d3","FileAttachment"], async function(d3,FileAttachment){return(
+Object.assign(d3.csvParse(await FileAttachment("alphabet.csv").text(), ({letter, frequency}) => ({name: letter, value: +frequency})).sort((a, b) => d3.descending(a.value, b.value)), {format: "%", y: "↑ Frequency"})
+)});
+  main.variable(observer("x")).define("x", ["d3","data","margin","width"], function(d3,data,margin,width){return(
+d3.scaleBand()
+    .domain(d3.range(data.length))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+)});
+  main.variable(observer("y")).define("y", ["d3","data","height","margin"], function(d3,data,height,margin){return(
+d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value)]).nice()
+    .range([height - margin.bottom, margin.top])
+)});
+  main.variable(observer("xAxis")).define("xAxis", ["height","margin","d3","x","data"], function(height,margin,d3,x,data){return(
+g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickFormat(i => data[i].name).tickSizeOuter(0))
+)});
+  main.variable(observer("yAxis")).define("yAxis", ["margin","d3","y","data"], function(margin,d3,y,data){return(
+g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y).ticks(null, data.format))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+        .attr("x", -margin.left)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text(data.y))
+)});
+  main.variable(observer("color")).define("color", function(){return(
+"steelblue"
+)});
+  main.variable(observer("height")).define("height", function(){return(
+500
+)});
+  main.variable(observer("margin")).define("margin", function(){return(
+{top: 30, right: 0, bottom: 30, left: 40}
+)});
+  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
+require("d3@6")
+)});
+  return main;
 }
-
-var cnvs = null;
-var data = new Map();
-var pie = null;
-
-function setup() {
-	cnvs = createCanvas(windowWidth, windowHeight);
-	pixelDensity(displayDensity());
-	smooth();
-  colorMode(HSB, 359, 99, 99);
-	data.set('Alpha', random(1, 50));
-	data.set('Bravo', random(1, 50));
-	data.set('Charlie', random(1, 50));
-	data.set('Delta', random(1, 50));
-	data.set('Echo', random(1, 50));
-	data.set('Foxtrot', random(1, 50));
-
-	pie = new PieChart(width * 0.5,
-			height * 0.5,
-			min(width, height) * 0.333,
-			"Example",
-			data);
-}
-
-function draw() {
-	background(0, 0, 0);
-	pie.draw();
-}
-
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-	pie.scale(width * 0.5, height * 0.5,
-		min(width, height) * 0.333);
-}
-
